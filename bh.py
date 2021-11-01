2733071111adb588133419577c7b24718b8c5697
idev-a <ideveloper003@protonmail.com>
2021-11-01 03:52:26 -0700

    f

--- bh.py
+++ bh.py
@@ -124,9 +124,9 @@ class Script:
             cats.append(cat)
             obj = s3.get_object(Bucket=ZORO_RESULTS_BUCKET, Key=file['Key'])
             dd = pd.read_excel(BytesIO(obj.get('Body').read()), sheet_name='Sheet', usecols=['category'])
-            if dd.empty:
+            if dd.empty or dd.isnull().values.any():
                 retry_cats.append(cat)
-        
+
         retry_cats += list(set(total_cats) - set(cats))
 
         print(retry_cats)
@@ -227,7 +227,7 @@ class Script:
 
         category_path = [ii.text.strip() for ii in sp1.select('nav.zcl-breadcrumb li span[itemprop="name"]')][1:]
         if not category_path:
-            category_path = [ii.text.strip() for ii in sp1.select('nav.zcl-breadcrumb li span[itemprop="name"]')]
+            category_path = [ii.text.strip() for ii in sp1.select('nav.Breadcrumb li span[itemprop="name"]')]
         mf_number=sp1.select_one('span[data-za="PDPMfrNo"]').text.strip()
         width=self._spec(sp1, 'Width')
         height=self._spec(sp1, 'Height')
@@ -252,12 +252,19 @@ class Script:
             on_old_list=self._check_old_list(mf_number)
         )
 
-    def fetch_zoro_data(self, cat_idx=0):
+    def list_zoro_cat(self, cat_idx=0):
         categories = self.request_with_retries(base_url).select('div.mega-menu > div.mega-menu__grid div.mega-menu__grid-item a.mega-menu__level1')
         logger.info(f"Total {len(categories)} categories")
         # for cat_url, cat in self.fetchList(categories):
         for x, link in enumerate(categories):
-            if x != int(cat_idx):
+            logger.info(f"[{x}] {self._url(link)}")
+
+    def fetch_zoro_data(self, cat_idx=0, cat_list=[]):
+        categories = self.request_with_retries(base_url).select('div.mega-menu > div.mega-menu__grid div.mega-menu__grid-item a.mega-menu__level1')
+        logger.info(f"Total {len(categories)} categories")
+        # for cat_url, cat in self.fetchList(categories):
+        for x, link in enumerate(categories):
+            if x not in cat_list:
                 continue
             cat_url = self._url(link)
             cat = self.request_with_retries(cat_url)
@@ -318,14 +325,21 @@ class Script:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--index', type=str, required=False, help="the name of childscraper. e.g, ganjapreneur from https://www.ganjapreneur.com/businesses/  complete example: python3 dirscraper.py -k ganjapreneur")
+    cat_idx = parser.parse_args().index or 0
 
+    cat_list = [0, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 22, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268, 269, 27, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285, 286, 287, 288, 289, 290, 291, 292, 293, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]
     script = Script()
     # script._save_master_excel()
-    cat_idx = parser.parse_args().index or 0
-    logger.info(f"{cat_idx}st Category scraper")
-    ZORO_PATH = BASE_PATH + f'/ZORO_SCRAPE_Category_{cat_idx}.xlsx'
-    with SgWriter(SgRecordDeduper(SgRecordID({SgRecord.Headers.ITEM_URL})), data_file=ZORO_PATH, s3=s3) as writer:
-        script.initialize()
-        results = script.fetch_zoro_data(cat_idx=cat_idx)
-        for rec in results:
-            writer.write_row(rec)
+    if cat_idx == 'missing':
+        script.find_missing_or_incomplete_categories()
+    elif cat_idx != '-1':
+        logger.info(f"{cat_idx}st Category scraper")
+        ZORO_PATH = BASE_PATH + f'/ZORO_SCRAPE_Category_{cat_idx}.xlsx'
+        with SgWriter(SgRecordDeduper(SgRecordID({SgRecord.Headers.ITEM_URL})), data_file=ZORO_PATH, s3=s3) as writer:
+            script.initialize()
+            results = script.fetch_zoro_data(cat_idx=cat_idx, cat_list=cat_list)
+            for rec in results:
+                writer.write_row(rec)
+    
+    elif cat_idx == 'list':
+        script.list_zoro_cat()
