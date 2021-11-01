12432f4a133317cb0480c0cc6b0a74d979b33fa2
idev-a <ideveloper003@protonmail.com>
2021-11-01 04:09:02 -0700

    f

--- bh.py
+++ bh.py
@@ -269,7 +269,7 @@ class Script:
             cat_url = self._url(link)
             cat = self.request_with_retries(cat_url)
             sub_categories = cat.select('ul.c-sidebar-nav__list li a')
-            logger.info(f"{cat_url} [sub cat {len(sub_categories)}]")
+            logger.warning(f"{cat_url} [sub cat {len(sub_categories)}]")
             if len(sub_categories):
                 # for sub_url, sub_cat in self.fetchList(sub_categories):
                 for sub_link in sub_categories:
